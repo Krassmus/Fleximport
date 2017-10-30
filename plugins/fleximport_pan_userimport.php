8aa573e2819c6857bf226eaafd90aa49cb22839d
Rasmus Fuhse <fuhse@data-quest.de>
2017-10-30 08:14:15 +0100

    change strings

--- plugins/fleximport_pan_userimport.php
+++ plugins/fleximport_pan_userimport.php
@@ -36,7 +36,7 @@ class fleximport_pan_userimport extends FleximportPlugin {
                     }
                     $datafield = DataField::findOneBySQL("name = 'Ich will weiterhin als Alumni in Stud.IP geführt werden' AND object_type = 'user'");
                     $user_wants_to_stay = DatafieldEntry::findOneBySQL("datafield_id = ? AND range_id = ?", array($datafield->getId(), $object->getId()));
-                    if ($user_wants_to_stay['content']) {
+                    if ($user_wants_to_stay && $user_wants_to_stay['content']) {
                         //In Veranstaltung ALUMNI die Statusgruppe anlegen:
                         $datafield = DataField::findOneBySQL("name = 'Alumni' AND object_type = 'user'");
                         $entry = DatafieldEntry::findOneBySQL("datafield_id = ? AND range_id = ?", array($datafield->getId(), $object->getId()));
