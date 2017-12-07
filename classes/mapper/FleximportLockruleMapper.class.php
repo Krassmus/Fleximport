6410bd57261c25b26e0ce1280460820902b16dc9
Rasmus Fuhse <fuhse@data-quest.de>
2017-12-07 15:33:57 +0100

    add lock-rules

---
+++ classes/mapper/FleximportLockruleMapper.class.php
@@ -0,0 +1,49 @@
+<?php
+
+class FleximportLockruleMapper implements FleximportMapper {
+
+    public function getName() {
+        return "Sperrebene";
+    }
+
+    public function possibleFieldnames() {
+        return array("lock_rule");
+    }
+
+    public function possibleFormats() {
+        $formats = array(
+            "name" => "Sperrebenenname",
+            "description" => "Beschreibung der Sperrebene",
+            "name_sem" => "Name der Sperrebene bei VA",
+            "name_user" => "Name der Sperrebene bei Nutzern",
+            "name_inst" => "Name der Sperrebene bei Einrichtungen"
+        );
+        return $formats;
+    }
+
+    public function map($format, $value, $data) {
+        switch ($format) {
+            case "name":
+                $lockrule = LockRule::findOneBySQL("name = ?", array($value));
+                return $lockrule ? $lockrule->getId() : null;
+                break;
+            case "description":
+                $lockrule = LockRule::findOneBySQL("description = ?", array($value));
+                return $lockrule ? $lockrule->getId() : null;
+                break;
+            case "name_sem":
+                $lockrule = LockRule::findOneBySQL("object_type = 'sem' AND name = ?", array($value));
+                return $lockrule ? $lockrule->getId() : null;
+                break;
+            case "name_user":
+                $lockrule = LockRule::findOneBySQL("object_type = 'user' AND name = ?", array($value));
+                return $lockrule ? $lockrule->getId() : null;
+                break;
+            case "name_inst":
+                $lockrule = LockRule::findOneBySQL("object_type = 'inst' AND name = ?", array($value));
+                return $lockrule ? $lockrule->getId() : null;
+                break;
+        }
+    }
+
+}
