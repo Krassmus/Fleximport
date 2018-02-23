c693348303a1d216ca4251b7c3bc7204f333e33b
Rasmus Fuhse <fuhse@data-quest.de>
2018-02-23 10:43:26 +0100

    bugfixing 4.0

--- classes/FleximportTable.php
+++ classes/FleximportTable.php
@@ -596,31 +596,6 @@ class FleximportTable extends SimpleORMap {
             }
         }
 
-        if ($classname === "Course") {
-            $folder_exist = DBManager::get()->prepare("
-                SELECT 1 FROM folder WHERE range_id = ?
-            ");
-            $folder_exist->execute(array($object->getId()));
-            if (!$folder_exist->fetch()) {
-                $insert_folder = DBManager::get()->prepare("
-                    INSERT IGNORE INTO folder
-                    SET folder_id = MD5(CONCAT(:seminar_id, 'allgemeine_dateien')),
-                    range_id = :seminar_id,
-                    user_id = :user_id,
-                    name = :name,
-                    description = :description,
-                    mkdate = UNIX_TIMESTAMP(),
-                    chdate = UNIX_TIMESTAMP()
-                ");
-                $insert_folder->execute(array(
-                    'seminar_id' => $object->getId(),
-                    'user_id' => $GLOBALS['user']->id,
-                    'name' => _("Allgemeiner Dateiordner"),
-                    'description' => _("Ablage für allgemeine Ordner und Dokumente der Veranstaltung")
-                ));
-            }
-        }
-
         if ($plugin && !$object->isNew()) {
             $plugin->afterUpdate($object, $line);
         }
