a227a4e7f12c44bf8e78b16bd3cf89b950410a62
Rasmus Fuhse <fuhse@data-quest.de>
2018-07-03 16:53:16 +0200

    add more statusgruppe_id mapper options

--- classes/mapper/FleximportStatusgruppe_idMapper.class.php
+++ classes/mapper/FleximportStatusgruppe_idMapper.class.php
@@ -12,16 +12,42 @@ class FleximportStatusgruppe_idMapper implements FleximportMapper {
 
     public function possibleFormats() {
         return array(
-            "uniquename" => "Gruppenname in Kontext"
+            "uniquename" => "Gruppenname in range_id",
+            "uniquename_semname" => "Gruppenname in Veranstaltungsname (Spalte kontext)",
+            "uniquename_semnumber" => "Gruppenname in Veranstaltungsnummer (Spalte kontext)",
+            "uniquename_inst" => "Gruppenname in Einrichtungsname (Spalte kontext)"
         );
     }
 
     public function map($format, $value, $data) {
-        if ($format === "uniquename") {
-            $gruppe = Statusgruppen::findOneBySQL("name = ? AND range_id = ?", array($value, $data['range_id']));
-            if ($gruppe) {
-                return $gruppe->getId();
-            }
+        switch ($format) {
+            case "uniquename":
+                $gruppe = Statusgruppen::findOneBySQL("name = ? AND range_id = ?", array($value, $data['range_id']));
+                if ($gruppe) {
+                    return $gruppe->getId();
+                }
+                break;
+            case "uniquename_semname":
+                $range = Course::findOneBySQL("name = ?", array($data['kontext']));
+                $gruppe = Statusgruppen::findOneBySQL("name = ? AND range_id = ?", array($value, $range->getId()));
+                if ($gruppe) {
+                    return $gruppe->getId();
+                }
+                break;
+            case "uniquename_semnumber":
+                $range = Course::findOneBySQL("VeranstaltungsNummer = ?", array($data['kontext']));
+                $gruppe = Statusgruppen::findOneBySQL("name = ? AND range_id = ?", array($value, $range->getId()));
+                if ($gruppe) {
+                    return $gruppe->getId();
+                }
+                break;
+            case "uniquename_semname":
+                $range = Institute::findOneBySQL("name = ?", array($data['kontext']));
+                $gruppe = Statusgruppen::findOneBySQL("name = ? AND range_id = ?", array($value, $range->getId()));
+                if ($gruppe) {
+                    return $gruppe->getId();
+                }
+                break;
         }
     }
 
