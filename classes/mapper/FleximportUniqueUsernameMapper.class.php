d6bf86839686560cc00cf8a67abab184d9016deb
Rasmus Fuhse <fuhse@data-quest.de>
2021-05-20 16:17:29 +0200

    add unique username mapper

---
+++ classes/mapper/FleximportUniqueUsernameMapper.class.php
@@ -0,0 +1,36 @@
+<?php
+
+class FleximportUniqueUsernameMapper implements FleximportMapper
+{
+
+    public function getName()
+    {
+        return "eindeutiger Nutzername";
+    }
+
+    public function possibleFieldnames()
+    {
+        return array("username");
+    }
+
+    public function possibleFormats()
+    {
+        $formats = array(
+            "uniqueusername" => "eindeutiger Nutzername"
+        );
+        return $formats;
+    }
+
+    public function map($format, $value, $data, $sormclass)
+    {
+        if (!User::findByUsername($value)) {
+            return $value;
+        }
+        $i = 0;
+        do {
+            $i++;
+        } while(User::findByUsername($value.$i));
+        return $value.$i;
+    }
+
+}
